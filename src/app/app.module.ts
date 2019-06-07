d74e18d6409b7b7863c0552db040dc2e639b1fb8
daniel.kostov <daniel.kostov@WS291>
2019-06-07 10:32:25 +0300

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { CharGroupComponent } from './char-group/char-group.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageItemComponent } from './Messages/message-item/message-item.component';
+import { MessageListComponent } from './Messages/message-list/message-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    CharGroupComponent,
+    MessagesComponent,
+    MessageItemComponent,
+    MessageListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/char-group/char-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-char-group',
+  templateUrl: './char-group.component.html',
+  styleUrls: ['./char-group.component.css']
+})
+export class CharGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
