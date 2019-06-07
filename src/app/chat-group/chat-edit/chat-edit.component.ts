f06601eb2f35d8a38a3f4ca912cb3ed2908d14d4
daniel.kostov <daniel.kostov@WS291>
2019-06-07 12:06:43 +0300

    chat group

---
+++ src/app/chat-group/chat-edit/chat-edit.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ChatGroup } from 'src/models/ChatGroup';
+import { GroupService } from 'src/app/shared/group.service';
+
+@Component({
+  selector: 'app-chat-edit',
+  templateUrl: './chat-edit.component.html',
+  styleUrls: ['./chat-edit.component.css']
+})
+export class ChatEditComponent implements OnInit {
+  @Input() public chatGroup: ChatGroup;
+  @Output() public saved: EventEmitter<ChatGroup> = new EventEmitter<ChatGroup>();
+
+  constructor(private chatGroupService: GroupService) { }
+
+  ngOnInit() {
+  }
+
+  save() {
+    this.saved.emit(this.chatGroup);
+  }
+
+}
--- src/app/chat-group/chat-group.component.ts
+++ src/app/chat-group/chat-group.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatGroup } from 'src/models/ChatGroup';
+import { GroupService } from '../shared/group.service';
 
 @Component({
   selector: 'app-chat-group',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatGroupComponent implements OnInit {
 
-  constructor() { }
+  public chatGroups: ChatGroup[];
+  public currentGroup: ChatGroup;
+
+  constructor(private groupService: GroupService) { }
 
   ngOnInit() {
+    this.currentGroup = {
+      id: null,
+      name: ''
+    };
   }
 
+  loadChatGroups() {
+    this.groupService.getAllGroups().subscribe(
+      (groups: ChatGroup[]) => this.chatGroups = groups
+    );
+  }
+
+  selectGroup(group: ChatGroup) {
+    this.currentGroup = group;
+  }
+
+  chatGroupSaved(group: ChatGroup) {
+    if (group.id) {
+      //save
+    } else {
+      this.groupService.createGroup(group).subscribe(
+        () => this.loadChatGroups()
+      );
+    }
+  }
 }
