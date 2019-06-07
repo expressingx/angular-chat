57e9747b1c606683b94a54d8bfbbb27edcdbc2cb
Ioan <j.stavrev@yahoo.com>
2019-06-07 11:43:53 +0300

    added timeStamp filtration to group messages

--- src/app/shared/group.service.ts
+++ src/app/shared/group.service.ts
@@ -14,10 +14,10 @@ export class GroupService {
         private http: HttpClient
     ) { }
 
-    async getAllMessages(groupId: string) {
+    async getAllMessages(groupId: string, timeStamp: Date) {
         let msg = await this.http.get<Message[]>(this.BASE_MESSAGE_URL).toPromise();
 
-        return msg.filter(m => m.groupId == groupId);
+        return msg.filter(m => m.groupId == groupId  && new Date(m.timeStamp) > timeStamp);
     }
 
     getAllGroups(): Observable<ChatGroup[]> {
