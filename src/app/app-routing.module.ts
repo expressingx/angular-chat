f73cf798e7b70bc5361057bdbcfcdf7f1f90aa2e
daniel.kostov <daniel.kostov@WS291>
2019-06-07 11:22:14 +0300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { MessagesComponent } from './messages/messages.component';
-import { CharGroupComponent } from './chat-group/chat-group.component';
+import { ChatGroupComponent } from './chat-group/chat-group.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
-  {path: 'chat-group', component: CharGroupComponent},
+  {path: 'chat-group', component: ChatGroupComponent},
   {path: 'messages', component: MessagesComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
